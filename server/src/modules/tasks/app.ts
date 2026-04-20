0e109db4e57fbe8a17c2d537456b93363e01c172
nikki <cozercaniegirl143@mail.ru>
2026-04-21 00:42:53 +0300

    fix: add CORS for access-control-allow-origin in browser

--- server/src/modules/tasks/app.ts
+++ server/src/modules/tasks/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import tasksRouter from './tasks.route.ts';
+import cors from 'cors';
 
 const PORT = '3000';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', tasksRouter);
 

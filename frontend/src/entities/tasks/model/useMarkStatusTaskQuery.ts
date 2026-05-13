f18ad2ce0badde10bc0e0d7adcc6d6420060ab59
nikki <cozercaniegirl143@mail.ru>
2026-05-13 19:28:54 +0300

    chore: add toaster for checkbox in the task

--- frontend/src/entities/tasks/model/useMarkStatusTaskQuery.ts
+++ frontend/src/entities/tasks/model/useMarkStatusTaskQuery.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { taskApi } from "../api/taskApi";
 import type { TaskType } from "./types";
+import { toaster } from "../../../shared/lib/ui/toaster";
 
 export function useMarkStatusQuery(task: TaskType) {
 
@@ -10,8 +11,11 @@ export function useMarkStatusQuery(task: TaskType) {
         mutationFn: (task: TaskType) => taskApi.updateTask(task.id.toString(), task),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['todo', task.id] });
-        },
-        onError: () => console.log('задачу не удалось изменить')
+            toaster.create({
+                title: 'Поздравляю с выполнением!',
+                type: 'success'
+            });
+        }
     });
 
     const changeCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {

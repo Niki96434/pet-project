99d2b05f5a3ffb455cf96eef3570248999af5013
nikki <cozercaniegirl143@mail.ru>
2026-05-27 20:04:16 +0300

    chore: add boilerplate for auth

---
+++ frontend/src/pages/login/ui/RegisterPage.tsx
@@ -0,0 +1,16 @@
+export function RegisterPage() {
+
+
+    return (
+        <form onSubmit={(e) => (new FormData(e.target))}>
+            <label htmlFor="email">Username</label>
+            <input id="email" name="email" required />
+
+            <label htmlFor="password">Password (min. 6 characters)</label>
+            <input id="password" name="password" type="password" required />
+
+            <label htmlFor="confirmPassword">Confirm password</label>
+            <input id="confirmPassword" name="confirmPassword" type="password" required />
+        </form>
+    )
+}
--- frontend/src/pages/tasks-page/ui/TasksPage.tsx
+++ frontend/src/pages/tasks-page/ui/TasksPage.tsx
@@ -1,4 +1,4 @@
-import DragAndDrop from './../../../features/tasks/drag-and-drop/ui/DragAndDrop';
+import DragAndDrop from '../../../features/tasks/drag-and-drop/ui/DragAndDrop';
 
 export function TasksPage() {
     return (

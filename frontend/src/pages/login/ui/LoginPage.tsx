c96d114935aee1805e9db523928e4cabd711e2ee
nikki <cozercaniegirl143@mail.ru>
2026-05-28 13:22:55 +0300

    feat: corrected login form

--- frontend/src/pages/login/ui/LoginPage.tsx
+++ frontend/src/pages/login/ui/LoginPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router";
 import { useForm } from "react-hook-form";
 import { type RegisterUserDto } from "../../../entities/users/api/authApi";
 import './LoginPage.css';
+import { NavLink } from 'react-router';
 
 export function LoginPage() {
 
@@ -30,33 +31,41 @@ export function LoginPage() {
     }
 
     return (
-        <>
-            <h1>Войдите в свой профиль</h1>
-            <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
-                <input id="username" placeholder="Username" required {...register("username", {
-                    required: 'Поле обязательно должно быть заполнено'
-                })} />
-                {errors.username && <span>* {errors.username.message}</span>}
-                <input id="password" type="password" placeholder="Password" required {...register("password", {
-                    required: 'Обязательно должно быть заполнено',
-                    minLength: {
-                        value: 6,
-                        message: 'Минимум 6 символов'
-                    }
-                })} />
-                {errors.password && <span>* {errors.password.message}</span>}
-                <input id="confirmPassword" type="password" placeholder="Confirm password" required {...register("confirmPassword", {
-                    required: 'Обязательно должно быть заполнено',
-                    minLength: {
-                        value: 6,
-                        message: 'Минимум 6 символов'
-                    },
-                    validate: (value) => value === getValues('password') || 'Пароли не совпадают'
-                })} />
-                {errors.confirmPassword && <span>* {errors.confirmPassword.message}</span>}
-                <button className='log-btn' disabled={!isValid} type="submit">Sign up</button>
-            </form>
-        </>
+        <div className="container-for-register">
+            <div className="register">
+                <form className='register-form' onSubmit={handleSubmit(onSubmit)}>
+                    <h1>Вход в профиль</h1>
+                    <input id="username" placeholder="Username" required {...register("username", {
+                        required: 'Поле обязательно должно быть заполнено'
+                    })} />
+                    {errors.username && <span>* {errors.username.message}</span>}
+                    <input id="password" type="password" placeholder="Password" required {...register("password", {
+                        required: 'Обязательно должно быть заполнено',
+                        minLength: {
+                            value: 6,
+                            message: 'Минимум 6 символов'
+                        }
+                    })} />
+                    {errors.password && <span>* {errors.password.message}</span>}
+                    <input id="confirmPassword" type="password" placeholder="Confirm password" required {...register("confirmPassword", {
+                        required: 'Обязательно должно быть заполнено',
+                        minLength: {
+                            value: 6,
+                            message: 'Минимум 6 символов'
+                        },
+                        validate: (value) => value === getValues('password') || 'Пароли не совпадают'
+                    })} />
+                    {errors.confirmPassword && <span>* {errors.confirmPassword.message}</span>}
+                    <button className='reg-btn' disabled={!isValid} type="submit">Log in</button>
+                    <div className="flex-or">
+                        <div className="around-line"></div>
+                        <p className="centre-or">OR</p>
+                        <div className="around-line"></div>
+                    </div>
+                    <NavLink to={'/'}><button className='reg-btn' type="submit">Sign up</button></NavLink>
+                </form>
+            </div>
+        </div>
     )
 }
 
